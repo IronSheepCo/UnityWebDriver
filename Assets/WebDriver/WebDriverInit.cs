854fe00c314f4a031583410d451f03ecdfb89673
Radu Dita <hades2510@yahoo.com>
2016-11-17 16:46:46 +0200

    added testing stage and init script

---
+++ Assets/WebDriver/WebDriverInit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace tech.ironsheep.WebDriver
+{
+	public class WebDriverInit : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+			//init the webdriver server
+			var instance = WebDriverManager.instance;
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+}
