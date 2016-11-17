462aa4a66cd386a7d0b3148871c38b629641c767
Radu Dita <hades2510@yahoo.com>
2016-11-17 18:32:48 +0200

    added main logic for commands

--- Assets/WebDriver/WebDriverManager.cs
+++ Assets/WebDriver/WebDriverManager.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
+using System.Threading;
+using System.IO;
 
 namespace tech.ironsheep.WebDriver
 {
@@ -12,6 +16,7 @@ namespace tech.ironsheep.WebDriver
 
 		private HttpListener listener;
 
+		private string sessionId = null;
 
 		private WebDriverManager()
 		{
@@ -19,7 +24,57 @@ namespace tech.ironsheep.WebDriver
 
 			listener.Prefixes.Add ("http://*:8080/");
 
+			StartWebDriver ();
+		}
+
+		private void StartWebDriver()
+		{
 			listener.Start ();
+
+			ThreadPool.QueueUserWorkItem ((_) => {
+				while( true )
+				{
+					HttpListenerContext ctx = listener.GetContext();
+
+					ThreadPool.QueueUserWorkItem( (idontcare)=>{
+						string command = ctx.Request.Url.Segments[1].Replace("/","");
+
+						var request = ctx.Request;
+
+						var response = ctx.Response;
+
+						var body = new StreamReader( request.InputStream ).ReadToEnd();
+
+						var args = request.Url.Segments.Skip(2);
+
+						switch( command ){
+						case "status":
+							break;
+						case "session":
+							switch( args.Count() ){
+							case 0:
+								switch( request.HttpMethod ){
+								case "GET":
+									break;
+								case "DELETE":
+									break;
+								default:
+									
+									break;
+								}
+								break;
+							case 1:
+								break;
+							default:
+								break;
+							}
+							break;
+						default:
+								break;
+						}
+					});
+				}
+			});
 		}
 
 		public static WebDriverManager instance {
@@ -37,6 +92,10 @@ namespace tech.ironsheep.WebDriver
 
 				return _instance;
 			}
+		}
+
+		public void Shutdown(){
+			listener.Stop ();
 		}
 	}
 }
