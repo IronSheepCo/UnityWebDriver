cc64139f82c1cb3e3bef394da595f9716ff0cf80
Radu Dita <hades2510@yahoo.com>
2016-11-17 15:29:55 +0200

    added webdrivermanager

---
+++ Assets/WebDriver/WebDriverManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace tech.ironsheep.WebDriver
+{
+	public class WebDriverManager
+	{
+		private static WebDriverManager _instance;
+
+		private WebDriverManager()
+		{
+			
+		}
+
+		public static WebDriverManager instance {
+			get{
+				if (_instance == null) 
+				{
+					lock (_instance) 
+					{
+						if (_instance == null) 
+						{
+							_instance = new WebDriverManager ();
+						}
+					}
+				}
+
+				return _instance;
+			}
+		}
+	}
+}
