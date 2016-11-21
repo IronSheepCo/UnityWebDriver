c55ecd308b0a47e9279b88f0fc6ecd636819141f
Radu Dita <hades2510@yahoo.com>
2016-11-21 17:37:25 +0200

    added method for parsing the requests' body

--- Assets/WebDriver/FindElementCommands.cs
+++ Assets/WebDriver/FindElementCommands.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Net;
 using System;
 
+using tech.ironsheep.WebDriver.Command;
+
 namespace tech.ironsheep.WebDriver
 {
 	public class FindElementCommands {
@@ -13,9 +15,46 @@ namespace tech.ironsheep.WebDriver
 			WebDriverManager.instance.RegisterCommand ("element", "POST", FindElement);
 		}
 
+		private static Command.FindBody ParseFindElementBody( string body, HttpListenerResponse response )
+		{
+			var parsedBody = SimpleJSON.JSON.Parse( body );
+
+			FindBody findBody = new FindBody ();
+
+			try{
+				findBody.locationStrategy = parsedBody ["using"];
+			}
+			catch( Exception e ) {
+				WebDriverManager.instance.InvalidArgument (response);
+				return null;
+			}
+
+			try{
+				findBody.selector = parsedBody ["value"];
+			}
+			catch(Exception e ){
+				WebDriverManager.instance.InvalidArgument (response);
+				return null;
+			}
+
+			//we only support 'css selector' and 'xpath' strategies
+			if (findBody.locationStrategy != "css selector" && findBody.locationStrategy != "xpath") 
+			{
+				//return an empty set
+				WebDriverManager.instance.WriteResponse( response, "{\"data\":null}", 200 );
+				return null;
+			}
+
+
+			return findBody;
+		}
+
 		public static bool FindElement( string body, string[] args, HttpListenerResponse response )
 		{
-			var findBody = SimpleJSON.JSON.Parse (body);
+			if (ParseFindElementBody (body, response) == null) 
+			{
+				return true;
+			}
 
 			return true;
 		}
