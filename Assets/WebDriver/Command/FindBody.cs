6e37b6240b7b801a6f9299dbb3a59634be42dbba
Radu Dita <hades2510@yahoo.com>
2016-11-21 14:16:14 +0200

    added class for find elements body

---
+++ Assets/WebDriver/Command/FindBody.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace tech.ironsheep.WebDriver.Command
+{
+	public class FindBody {
+		public string locationStrategy;
+		public string selector;
+	}
+}
