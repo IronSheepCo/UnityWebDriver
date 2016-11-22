6f848617ba73b082d8d791a9b46674e335ba2595
Radu Dita <hades2510@yahoo.com>
2016-11-22 13:20:48 +0200

    added predicate for number

---
+++ Assets/WebDriver/XPath/XPathNumberPredicate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace tech.ironsheep.WebDriver.XPath
+{
+	public class XPathNumberPredicate: XPathPredicate
+	{
+		public int Number;
+
+		override public List<GameObject> Evaluate( List<GameObject> nodeList, string scriptName )
+		{
+			if (nodeList.Count <= Number) 
+			{
+				return null;
+			}
+
+			List<GameObject> ret = new List<GameObject> ();
+			ret.Add (nodeList [Number]);
+
+			return ret;
+		}
+	}
+}
