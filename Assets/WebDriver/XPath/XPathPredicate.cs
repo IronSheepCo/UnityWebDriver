77c90401c006f7d722a6623ad0c3d9a08f975352
Radu Dita <hades2510@yahoo.com>
2016-11-22 12:46:14 +0200

    added predicate for xpath parser

---
+++ Assets/WebDriver/XPath/XPathPredicate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace tech.ironsheep.WebDriver.XPath
+{
+	public class XPathPredicate
+	{
+		virtual public List<GameObject> Evaluate( List<GameObject> set, string scriptName )
+		{
+			return set;
+		}
+	}
+}
