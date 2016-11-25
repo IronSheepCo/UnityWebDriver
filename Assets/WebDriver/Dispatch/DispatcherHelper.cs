a170195b98485d2fdf685cab8c032d093bb4aa13
Radu Dita <hades2510@yahoo.com>
2016-11-25 16:30:32 +0200

    added dispatcher for executing actions on main thread

---
+++ Assets/WebDriver/Dispatch/DispatcherHelper.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DispatcherHelper : MonoBehaviour {
+
+	private List<Action> actions = new List<Action>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lock (actions) {
+			
+			foreach (Action ac in actions) {
+				ac ();
+			}
+
+			actions = new List<Action> ();
+		}
+	}
+
+	public void EnqueAction( Action ac )
+	{
+		lock (actions) {
+			actions.Add (ac);
+		}
+	}
+}
---
+++ Assets/WebDriver/Dispatch/MainDispatcher.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace tech.ironsheep.WebDriver.Dispatch
+{
+	public class MainDispatcher{
+		//use to force blocking behaviour
+		private static Dictionary<string, bool> busyWaitingFlags = new Dictionary<string, bool> ();
+
+		private static DispatcherHelper realDispatcher;
+
+		private static Thread uiThread;
+
+		static MainDispatcher()
+		{
+			var go = new GameObject ("ui dispatcher");
+			go.AddComponent<DispatcherHelper> ();
+
+			GameObject.DontDestroyOnLoad (go);
+
+			realDispatcher = go.GetComponent<DispatcherHelper>();
+
+			uiThread = System.Threading.Thread.CurrentThread;
+		}
+
+		public static void ExecuteBlocking( Action ac )
+		{
+			//check if we're on the main thread
+			if (System.Threading.Thread.CurrentThread == uiThread) 
+			{
+				ac ();
+				return;
+			}
+
+			string uid = System.Guid.NewGuid ().ToString();
+
+			busyWaitingFlags [uid] = false;
+
+			realDispatcher.EnqueAction (() => {
+				ac();
+				busyWaitingFlags[uid] = true;
+			});
+
+			//busy waiting
+			while (busyWaitingFlags [uid] == false) 
+			{
+			}
+
+			//done
+		}
+
+	}
+}
