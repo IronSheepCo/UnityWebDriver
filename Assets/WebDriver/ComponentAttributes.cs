0f8b8bf53fcebd775dfbc06c1bbfb4f47c7148d3
Radu Dita <hades2510@yahoo.com>
2017-01-17 16:05:08 +0200

    added class for finding fields and properites on Components

---
+++ Assets/WebDriver/ComponentAttributes.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace tech.ironsheep.WebDriver
+{
+	public class ComponentAttributes 
+	{
+		public static Dictionary<string,string> Attributes( Type t, object obj )
+		{
+			Dictionary<string, string> ret = new Dictionary<string, string> ();
+
+			//getting properties
+			PropertyInfo[] properties = t.GetProperties (BindingFlags.Instance | BindingFlags.Public );
+
+			foreach (PropertyInfo info in properties) 
+			{
+				if (info.CanRead) 
+				{
+					try
+					{
+						ret[ info.Name ] =  info.GetValue (obj, null).ToString();
+					}
+					catch(Exception e) 
+					{
+						
+					}
+				}
+			}
+
+			//getting fields
+			FieldInfo[] fields = t.GetFields (BindingFlags.Instance | BindingFlags.Public);
+
+			foreach (FieldInfo fi in fields) 
+			{
+				try
+				{
+					ret [fi.Name] = fi.GetValue (obj).ToString ();
+				}
+				catch(Exception e)
+				{
+					
+				}
+			}
+
+			return ret;
+		}
+	}
+}
